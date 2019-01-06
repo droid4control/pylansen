cd243534e02add092e074ed6f7b0806499804273
Cougar <cougar@random.ee>
2019-01-07 01:09:01 +0200

    Show unknown data in debug

--- pylansen/enapimbusdata.py
+++ pylansen/enapimbusdata.py
@@ -1,6 +1,8 @@
 from .enapicommandwithsvn import ENAPICommandWithSvn
 from .exceptions import ENAPILenException
 
+import binascii
+
 import logging
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -19,18 +21,24 @@ class ENAPIMbusData(ENAPICommandWithSvn):
 
         # FIXME: if medium == 0x32 (Unidirectional Repeater), remove 3 unknown bytes and fix frame length
         if len(self.MbusData) > 9 and self.MbusData[9] == 0x32:
+            log.debug("MbusData(%d): %s", len(self.MbusData), binascii.hexlify(self.MbusData))
             # remove unknown data
             self._unknown_data = self.MbusData[-3:]
             del self.MbusData[-3:]
             # fix L-field
             self.MbusData[0] = len(self.MbusData)
+            log.debug("MbusData(%d): %s", len(self.MbusData), binascii.hexlify(self.MbusData))
+            log.debug("remaining(%d): %s", len(self._unknown_data), binascii.hexlify(self._unknown_data))
 
         # FIXME: this frame comes from repeater? some unknown data needs to be removed
         if self.MbusData[0] < len(self.MbusData):
             log.info("L-field=%d but data len=%d, fixing..", self.MbusData[0], len(self.MbusData))
+            log.debug("MbusData(%d): %s", len(self.MbusData), binascii.hexlify(self.MbusData))
             self._unknown_data = self.MbusData[self.MbusData[0]:]
             self.MbusData = self.MbusData[:self.MbusData[0]]
 
+            log.debug("MbusData(%d): %s", len(self.MbusData), binascii.hexlify(self.MbusData))
+            log.debug("remaining(%d): %s", len(self._unknown_data), binascii.hexlify(self._unknown_data))
     @property
     def MbusData(self):
         return self._MbusData
